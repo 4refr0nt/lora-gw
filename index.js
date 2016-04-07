1d62dab707812343995ef43e968840691bb3d417
Alex Suslov <suslov@me.com>
2016-04-07 20:15:28 +0300

    modules

--- index.js
+++ index.js
@@ -22,6 +22,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+"use restrict";
 ////////////////////////////////////////////////
 var config = require("./config");
 var host = config.host;
@@ -49,7 +50,7 @@ t.reset = function(n) {
     rx.dir(mraa.DIR_OUT);
     rx.write(rx_en, 0);
   }
-    
+
 }
 t.init = function(n) {
         console.log('LoRa Gateway Version: ' + version + ' started.');
